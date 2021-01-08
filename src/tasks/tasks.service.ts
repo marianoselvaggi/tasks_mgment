369d8502a33fd52763e48b505452df8c40909036
MARIANO SELVAGGI <mariano_selvaggi@yahoo.com.ar>
2021-01-08 15:59:12 -0300

    add task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class TasksService {
-    private tasks = [];
+    private tasks: Task[] = [];
 
-    getAllTasks() {
+    getAllTasks(): Task[] {
         return this.tasks;
+    }
+
+    createTask(title: string, description: string): Task {
+        const task: Task = {
+            title,
+            description,
+            status: TaskStatus.OPEN,
+            id: uuidv4()    
+        };
+
+        this.tasks.push(task);
+
+        return task;
     }
 }
